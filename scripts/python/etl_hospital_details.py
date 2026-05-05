715791e06c6b7343b9f864cf8a324a44410fade7
Deborah I. <24484926+TheLadyDothCodeTooMuch@users.noreply.github.com>
2026-05-05 19:18:20 +0100

    Refactor ETL script to include ASSOCIATE ID

--- scripts/python/etl_hospital_details.py
+++ scripts/python/etl_hospital_details.py
@@ -26,7 +26,7 @@ FILE_NAMES = {
 }
 
 
-PATH = r"C:\Users\chika\OneDrive\Desktop\Project\Healthcare\datasets\\"
+PATH = r"...\Project\Healthcare\datasets\\"
 
 def main():
     engine = get_engine()
@@ -57,7 +57,7 @@ def main():
        
 def get_engine() -> Engine:
     connection_url = (
-        "mssql+pyodbc://Chika\\SQLEXPRESS/NCEH_Clinical_Operations_2026?"
+        "mssql+pyodbc://...\\SQLEXPRESS/NCEH_Clinical_Operations_2026?"
         "driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
     )
     return create_engine(connection_url, fast_executemany=True)
